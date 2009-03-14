0f55cb77cfd47a538dfa65892cc2183d0d1c216a
CORP\thanley <corpthanley@mtvl08w001094.(none)>
2009-03-13 19:20:20 -0700

    got the method triggering on controller action

--- lib/js_loggable.rb
+++ lib/js_loggable.rb
@@ -1,8 +1,25 @@
 # JsLoggable
 module JsLoggable
+  def self.included(controller)
+    controller.extend(ClassMethods)
+    controller.before_filter(:make_javascript_loggable)
+  end                                       
+  module ClassMethods
+    def log_javascript(*actions)
+      write_inheritable_array(:log_javascript_actions, actions)
+    end
+  end
   
-   def self.log_javascript
-     $stdout.puts "request made"
-   end
-   
+  protected
+    # Returns true if the current action is supposed to run as SSL
+    def js_loggable?
+      (self.class.read_inheritable_attribute(:log_javascript_actions) || []).include?(action_name.to_sym)
+    end
+  
+  private
+    def make_javascript_loggable
+      if js_loggable?
+         $stdout.puts "javascript log me"
+      end
+    end
 end
